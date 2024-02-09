15f6f3629ad4dde7093816e788ba399fcdb61e6b
eemelimu <ebe.22@hotmail.com>
2024-02-09 14:18:41 +0200

    Update dependencies and add back button to Paskasivu.js

--- components/Paskasivu.js
+++ components/Paskasivu.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const Paskasivu = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>Olet siirtynyt skldaf</Text>
+      <Button
+        title="Takaisin"
+        onPress={() => {
+          navigation.navigate("LoginScreen");
+        }}
+      ></Button>
     </View>
   );
 };

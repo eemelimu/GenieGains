b7e58c6eaebd3b52f4a6c7ebe6d86893266ac351
eemelimu <ebe.22@hotmail.com>
2024-02-09 14:03:09 +0200

    Navigation

---
+++ components/Paskasivu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Paskasivu = () => {
+  return (
+    <View>
+      <Text>Olet siirtynyt skldaf</Text>
+    </View>
+  );
+};
+
+export default Paskasivu;

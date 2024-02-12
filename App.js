a03e803a7e40f829c8f93a7efda0134a1b62510f
eemelimu <ebe.22@hotmail.com>
2024-02-12 20:51:15 +0200

    Home screen

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./components/LoginScreen";
 import HomeScreen from "./components/HomeScreen";
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
+const CustomHeader = ({ navigation }) => {
+  return (
+    <View style={styles.header}>
+      <SimpleLineIcons
+        name="menu"
+        size={30}
+        color="black"
+        style={{ marginLeft: 20 }}
+        onPress={() => navigation.openDrawer()}
+      />
+    </View>
+  );
+};
+
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* Vaihda initialRouteName ennen mergeä */}
-      <Stack.Navigator
-        initialRouteName="HomeScreen"
-        
-        // screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            header: ({ navigation }) => <CustomHeader navigation={navigation} />,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
@@ -26,8 +41,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  header: {
+    left: -20,
+    top: 50,
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 20,
+    paddingTop: 10,
   },
 });
-
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,20 +5,21 @@ import {
   Text,
   SafeAreaView,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import { ThemeColors } from "../assets/ThemeColors";
-import { Feather } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
   const [date] = useState(new Date());
-  const [greeting, setGreeting] = useState('');
-  const [name, setName] = useState('name');
+  const [greeting, setGreeting] = useState("");
+  const [name, setName] = useState("name");
 
   const dateToString = date.toLocaleDateString(undefined, {
-    weekday: "long",
-    month: "long",
+    weekday: "short",
+    month: "short",
     day: "numeric",
   });
 
@@ -39,68 +40,112 @@ const HomeScreen = () => {
   };
 
   // TODO: Hae käyttäjän nimi useEffectin avulla
-  
+
   useEffect(() => {
     const currentTime = new Date().getHours();
     if (currentTime < 12) {
-      setGreeting('Good morning');
+      setGreeting("Good morning");
     } else if (currentTime < 18) {
-      setGreeting('Good afternoon');
+      setGreeting("Good afternoon");
     } else {
-      setGreeting('Good evening');
+      setGreeting("Good evening");
     }
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-      <Text style={styles.date}>{dateToString.toUpperCase()}</Text>
-        <Text style={styles.greetings}>{greeting}, {name}!</Text>
+        <Text style={styles.date}>{dateToString.toUpperCase()}</Text>
+        <Text style={styles.greetings}>
+          {greeting}, {name}!
+        </Text>
       </View>
-      <View style={styles.main}>
-      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.main}>
+          <Workout name="Push" date={dateToString} />
+          <Workout name="Pull" date={dateToString} />
+          <Workout name="Legs" date={dateToString} />
+          <Workout name="Push" date={dateToString} />
+          <Workout name="Pull" date={dateToString} />
+          <Workout name="Legs" date={dateToString} />
+          <Workout name="Workout 7" date={dateToString} />
+          <Workout name="Workout 8" date={dateToString} />
+        </View>
+      </ScrollView>
 
       <View style={styles.footer}>
         <TouchableOpacity style={styles.footerButton} onPress={handleJournal}>
-          <MaterialCommunityIcons
-            name="file-document-multiple-outline"
-            size={24}
-            color="black"
-          />
-          <Text>Journal</Text>
+          <Entypo name="back-in-time" size={24} color="black" />
+          <Text>Log</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={[styles.footerButton, styles.startWorkoutButton]}
           onPress={handleNewWorkout}
         >
           <AntDesign name="plus" size={24} color="black" />
-          <Text style={{fontWeight: 'bold', fontSize: 15}}>Start Workout</Text>
+          <Text style={{ fontWeight: "bold", fontSize: 15 }}>
+            Start Workout
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.footerButton} onPress={handleSettings}>
-          <Feather name="settings" size={24} color="black" />
-          <Text>Settings</Text>
+        <TouchableOpacity style={styles.footerButton} onPress={handleJournal}>
+          <Ionicons name="stats-chart" size={24} color="black" />
+          <Text>Progress</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
 
+const Workout = ({ name, date }) => {
+  return (
+    <View style={styles.singleWorkout}>
+      <Text style={styles.workoutName}>{name}</Text>
+      <Text style={styles.workoutDate}>{date}</Text>
+      <Text>Days since last: -</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
+  singleWorkout: {
+    width: "90%",
+    height: 100,
+    backgroundColor: "#D8D8D8",
+    marginVertical: 7,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    position: "relative",
+  },
+  workoutName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+    alignSelf: "flex-start",
+    position: "absolute",
+    top: 10,
+    left: 10,
+  },
+  workoutDate: {
+    fontSize: 14,
+    color: "#666",
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
   container: {
     flex: 1,
   },
   main: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
+    flex: 1,
     alignItems: "center",
     paddingVertical: 20,
     paddingHorizontal: 20,
   },
   header: {
-    top: 0,
     paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 100,
   },
   date: {
     color: "#02075d",
@@ -112,7 +157,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   footer: {
-    position: "absolute",
     bottom: 0,
     width: "100%",
     height: 75,
@@ -124,12 +168,17 @@ const styles = StyleSheet.create({
   startWorkoutButton: {
     backgroundColor: ThemeColors.primary,
     borderRadius: 5,
-    padding: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: 150, // Fixed width for all buttons
+    alignItems: "center", // Center button content horizontally
   },
   footerButton: {
-    padding: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
     borderRadius: 5,
-    alignItems: "center",
+    width: 150, // Fixed width for all buttons
+    alignItems: "center", // Center button content horizontally
   },
 });
 

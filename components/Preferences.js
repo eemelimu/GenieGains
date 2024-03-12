fb6e19b52a82df4fd5e17071fb441c3fb3acabb1
eemelimu <eemelimu@outlook.com>
2024-03-12 13:49:21 +0200

    Update skill level selection UI in Preferences.js

--- components/Preferences.js
+++ components/Preferences.js
@@ -31,7 +31,7 @@ const Preferences = ({ route }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.preferenceText}>
-        To get started you need to choose your skill level!
+        Select your skill level to get started
       </Text>
       <View style={styles.checkboxContainer}>
         <TouchableOpacity
@@ -42,11 +42,17 @@ const Preferences = ({ route }) => {
             style={styles.checkbox}
             value={selectedSkill === "Beginner"}
             onValueChange={() => setSelectedSkill("Beginner")}
-            color={ThemeColors.tertiary}
+            color={
+              selectedSkill === "Beginner" ? "orange" : ThemeColors.tertiary
+            }
           />
-          <Text style={styles.skills}>Beginner</Text>
+          <View>
+            <Text style={styles.skills}>Beginner</Text>
+            <Text style={[styles.skills, { fontSize: 15, paddingTop: 10 }]}>
+              You are new to the gym with less than a year of experience.
+            </Text>
+          </View>
         </TouchableOpacity>
-
         <TouchableOpacity
           style={styles.checkboxItem}
           onPress={() => setSelectedSkill("Intermediate")}
@@ -55,9 +61,16 @@ const Preferences = ({ route }) => {
             style={styles.checkbox}
             value={selectedSkill === "Intermediate"}
             onValueChange={() => setSelectedSkill("Intermediate")}
-            color={ThemeColors.tertiary}
+            color={
+              selectedSkill === "Intermediate" ? "orange" : ThemeColors.tertiary
+            }
           />
-          <Text style={styles.skills}>Intermediate</Text>
+          <View>
+            <Text style={styles.skills}>Intermediate</Text>
+            <Text style={[styles.skills, { fontSize: 15, paddingTop: 10 }]}>
+              You have been lifing consistently for a few years.
+            </Text>
+          </View>
         </TouchableOpacity>
 
         <TouchableOpacity
@@ -68,23 +81,22 @@ const Preferences = ({ route }) => {
             style={styles.checkbox}
             value={selectedSkill === "Professional"}
             onValueChange={() => setSelectedSkill("Professional")}
-            color={ThemeColors.tertiary}
+            color={
+              selectedSkill === "Professional" ? "orange" : ThemeColors.tertiary
+            }
           />
-          <Text style={styles.skills}>Professional</Text>
+          <View>
+            <Text style={styles.skills}>Professional</Text>
+            <Text style={[styles.skills, { fontSize: 15, paddingTop: 10 }]}>
+              You have been lifing consistently for a many years, you know your
+              stuff and probably compete.
+            </Text>
+          </View>
         </TouchableOpacity>
       </View>
-      {/* <TouchableOpacity
-        style={styles.NextBtn}
-        onPress={() => {
-          moveToPreferences2();
-          console.log("tets?");
-        }}
-      >
-        <Text style={styles.NextBtnText}>Next</Text>
-      </TouchableOpacity> */}
       <Button
         textSize={22}
-        width={120}
+        width={250}
         height={50}
         text={"Next"}
         onPress={moveToPreferences2}
@@ -94,44 +106,48 @@ const Preferences = ({ route }) => {
 };
 
 const styles = StyleSheet.create({
+  boxContainer: {
+    width: "100%",
+    backgroundColor: ThemeColors.primary,
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
   container: {
     flex: 1,
     width: "100%",
-    backgroundColor: "#2F2F2F",
+    backgroundColor: ThemeColors.primary,
     alignItems: "center",
+    paddingHorizontal: 20,
   },
-
   checkboxContainer: {
     flexDirection: "column",
     marginBottom: 20,
   },
   checkboxItem: {
+    marginHorizontal: 20,
+    margin: 20,
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 70,
+    marginBottom: 20,
     color: ThemeColors.tertiary,
   },
-
   checkbox: {
     alignSelf: "center",
     width: 35,
     height: 35,
   },
-
   preferenceText: {
-    paddingTop: 10,
-    fontSize: 35,
-    marginBottom: 80,
+    paddingTop: 50,
+    fontSize: 25,
+    marginBottom: 40,
     color: ThemeColors.tertiary,
-    fontFamily: "DMBold",
-    paddingHorizontal: 20,
     textAlign: "center",
   },
-
   skills: {
-    fontSize: 30,
+    fontSize: 20,
+    paddingRight: 10,
     fontFamily: "DMRegular",
-    paddingLeft: 40,
+    paddingLeft: 20,
     color: ThemeColors.tertiary,
   },
   NextBtnText: {
@@ -144,4 +160,5 @@ const styles = StyleSheet.create({
     fontFamily: "DMBold",
   },
 });
+
 export default Preferences;

b204536c285ea052084fdd55cccadcabd51daa9f
vilppu <blaanblaa123@gmail.com>
2024-03-15 00:45:32 +0200

    fixed tests

--- __tests__/Preferences2.test.js
+++ __tests__/Preferences2.test.js
@@ -1,15 +1,13 @@
 import React from "react";
-import renderer from "react-test-renderer";
-import { act } from "react-dom/test-utils";
-import Preferences2 from "../components/Preferences2";
+import renderer, { act } from "react-test-renderer";
+import { waitFor, fireEvent, render } from "@testing-library/react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "../components/ThemeContext";
 import { AuthProvider } from "../components/AuthContext";
-import { waitFor, render, fireEvent } from "@testing-library/react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Login from "../components/Login";
-import { useNavigation } from "@react-navigation/native";
 import { NotificationProvider } from "../components/NotificationContext";
+import Preferences2 from "../components/Preferences2";
+import Login from "../components/Login";
 import Notification from "../components/Notification";
 import Toast, { ErrorToast } from "react-native-toast-message";
 
@@ -24,53 +22,48 @@ test("Preferences2 component renders correctly and that the theme context applie
       <NavigationContainer>
         <NotificationProvider>
           <ThemeProvider>
-            {" "}
             <AuthProvider>
               <Stack.Navigator>
                 <Stack.Screen name="Preferences2" component={Preferences2} />
                 <Stack.Screen name="Login" component={Login} />
               </Stack.Navigator>
+              <Toast />
               <Notification />
             </AuthProvider>
           </ThemeProvider>
-
-          <Toast />
         </NotificationProvider>
       </NavigationContainer>
     );
   });
 
   await waitFor(() => {
-    act(() => {
-      let tree;
-      tree = component.toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
 
-test("Next button can be pressed and works as expected", async () => {
+test("Register button can be pressed and works as expected", async () => {
   const Stack = createStackNavigator();
   let component;
-  component = render(
-    <NavigationContainer>
-      <NotificationProvider>
-        <ThemeProvider>
-          <AuthProvider>
-            <Stack.Navigator>
-              <Stack.Screen name="Preferences2" component={Preferences2} />
-              <Stack.Screen name="Login" component={Login} />
-            </Stack.Navigator>
-            <Notification />
-            <Toast />
-          </AuthProvider>
-        </ThemeProvider>
-      </NotificationProvider>
-    </NavigationContainer>
-  );
 
-  await waitFor(() => {
-    const nextButton = component.getByText("Register");
-    fireEvent.press(nextButton);
-  });
+ 
+    component = render(
+      <NavigationContainer>
+        <NotificationProvider>
+          <ThemeProvider>
+            <AuthProvider>
+              <Stack.Navigator>
+                <Stack.Screen name="Preferences2" component={Preferences2} />
+                <Stack.Screen name="Login" component={Login} />
+              </Stack.Navigator>
+              <Toast />
+              <Notification />
+            </AuthProvider>
+          </ThemeProvider>
+        </NotificationProvider>
+      </NavigationContainer>
+    );
+
+  const registerButton = component.getByText("Register");
+  fireEvent.press(registerButton);
 });
